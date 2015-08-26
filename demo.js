1bcce8ee6e752048a1d5e2da47ad08868c26ce68
Jhon Jimenez <jhonb3r@gmail.com>
2015-08-26 10:37:28 -0400

    Support user storage

--- demo.js
+++ demo.js
@@ -1,16 +1,40 @@
 
 module.exports = function (io) {
   var users = {};
-  var usersCnt  = 0;
+  var numUsers  = 0;
 
   io.on('connection', function (socket) {
-    console.log(socket)
+    var added = false;
+    console.log("new connection");
+
     socket.on('keyEvent', function (data) {
-      if (data.username == 'jhonber') {
-        socket.emit('keyEvent', data.key);
+      console.log(socket.username)
+      if (socket.username == 'jhonber') {
+        socket.broadcast.emit('keyEvent', data);
       }
     });
 
+    socket.on('new user', function (username) {
+      console.log('user added: ', username)
+
+      socket.username = username;
+      users[username] = username;
+      numUsers ++;
+      added = true;
+
+      socket.emit('login', {
+        ok: true,
+        numUsers: numUsers
+      });
+    });
+
+  });
+
+  io.on('disconnect', function() {
+    if (added) {
+      delete user[socket.username];
+      numUsers --;
+    }
   });
 
 }
